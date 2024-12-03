0def2e99459ef25b164dcd9380ed7d43e4af9a24
JenilPoria <poriajenil696@gmail.com>
2024-12-03 15:28:19 +0530

    stage 1 updated

--- src/CnnClassifier/utils/common.py
+++ src/CnnClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from CnnClassifier import logger
+from src.CnnClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
