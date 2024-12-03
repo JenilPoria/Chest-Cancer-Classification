60ba16667198f5466e7d137c302a61ba6e6efae8
JenilPoria <poriajenil696@gmail.com>
2024-12-04 01:04:42 +0530

    complete

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from src.CnnClassifier import logger
 from src.CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.CnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-# from src.CnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from src.CnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+import dagshub
+dagshub.init(repo_owner='JenilPoria', repo_name='Chest-Cancer-Classification', mlflow=True)
 
 
 
@@ -49,14 +51,14 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Evaluation stage"
-# try:
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_evalution = EvaluationPipeline()
-#    model_evalution.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+except Exception as e:
+        logger.exception(e)
+        raise e
